fd4cec414c77d9e8ff515c91227f097a8623a366
vae <18137693952@163.com>
2025-10-16 18:14:47 +0800

    chore: 更新打包文件及版本

--- dist/emoji.js
+++ dist/emoji.js
@@ -3,20 +3,20 @@ var r = (o, t, i) => t in o ? c(o, t, { enumerable: !0, configurable: !0, writab
 var s = (o, t, i) => r(o, typeof t != "symbol" ? t + "" : t, i);
 import a from "@emoji-mart/data";
 import { Picker as h } from "emoji-mart";
-const l = class l {
+const n = class n {
   constructor(t, i = {}) {
     s(this, "quill");
     s(this, "options");
     s(this, "picker");
     s(this, "isPickerVisible");
-    this.quill = t, this.options = { ...l.DEFAULTS, ...i }, this.picker = null, this.isPickerVisible = !1, this.init();
+    this.quill = t, this.options = { ...n.DEFAULTS, ...i }, this.picker = null, this.isPickerVisible = !1, this.init();
   }
   init() {
     const t = this.quill.getModule("toolbar");
     if (t && t.container) {
       const i = document.createElement("button");
       i.className = "ql-emoji", i.type = "button", i.title = "插入表情符号", t.container.appendChild(i), i.addEventListener("click", (e) => {
-        e.preventDefault(), e.stopPropagation(), this.isPickerVisible ? this.closeDialog() : (this.openDialog(), this.isPickerVisible = !0);
+        e.preventDefault(), e.stopPropagation(), this.isPickerVisible ? this.closeDialog() : this.openDialog();
       });
     }
   }
@@ -33,32 +33,28 @@ const l = class l {
         ...this.options
       }), document.body.appendChild(this.picker);
       const i = (t = this.getButton()) == null ? void 0 : t.getBoundingClientRect();
-      i && (this.picker.style.top = `${i.top + 25}px`, this.picker.style.left = `${i.left}px`), this.picker.style.boxShadow = "0 4px 4px 0 rgba(0, 0, 0, 0.25)", this.picker.style.position = "absolute", this.picker.style.zIndex = "1";
+      i && (this.picker.style.top = `${i.top + 25}px`, this.picker.style.left = `${i.left}px`), this.picker.style.boxShadow = "0 4px 4px 0 rgba(0, 0, 0, 0.25)", this.picker.style.position = "absolute", this.picker.style.zIndex = "1", this.isPickerVisible = !0;
     }
-  }
-  selectEmoji(t) {
-    const i = this.quill.insertText(
-      this.quill.getSelection(!0).index,
-      t.native,
-      "user"
-    );
-    this.closeDialog(), setTimeout(() => {
-      this.quill.setSelection(this.quill.getSelection(!0).index + i.length());
-    });
-  }
-  onClickOutside(t) {
-    const i = this.getButton();
-    (!i || !(i === t.target || t.target instanceof Element && i.contains(t.target))) && this.closeDialog();
   }
   closeDialog() {
     var t;
     this.isPickerVisible = !1, (t = this.picker) == null || t.remove(), this.picker = null;
   }
+  selectEmoji(t) {
+    const e = this.quill.getSelection(!0).index;
+    this.quill.insertText(e, t.native, "user"), this.closeDialog(), setTimeout(() => {
+      this.quill.setSelection(e + t.native.length);
+    }, 0);
+  }
+  onClickOutside(t) {
+    const i = this.getButton();
+    (!i || !(i === t.target || t.target instanceof Element && i.contains(t.target))) && this.closeDialog();
+  }
   destroy() {
     this.closeDialog();
   }
 };
-s(l, "DEFAULTS", {
+s(n, "DEFAULTS", {
   theme: "light",
   locale: "zh",
   set: "native",
@@ -72,7 +68,7 @@ s(l, "DEFAULTS", {
   noCountryFlags: !1,
   dynamicWidth: !1
 });
-let n = l;
+let l = n;
 export {
-  n as default
+  l as default
 };
