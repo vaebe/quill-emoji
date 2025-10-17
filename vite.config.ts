231a44086dfbd80c96d29b05b0782db78d7a32a5
vae <18137693952@163.com>
2025-10-17 11:40:47 +0800

    chore: 打包配置修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 /// <reference types="vitest" />
 import { resolve } from 'node:path'
-import camelCase from 'camelcase'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import packageJson from './package.json'
-
-const packageName = packageJson.name.split('/').pop() || packageJson.name
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es', 'cjs', 'umd'],
-      name: camelCase(packageName, { pascalCase: true }),
-      fileName: packageName,
+      name: 'QuillEmoji',
+      fileName: 'quill-emoji',
     },
     rollupOptions: {
       external: ['quill', 'emoji-mart', '@emoji-mart/data'],
